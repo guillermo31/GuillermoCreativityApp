e6fd6a3b32c547c5444ace24a0e03494f18aed5f
Guillermo Ramos <memo33101@gmail.com>
2018-11-03 12:58:30 -0600

    Initial Commit

---
+++ GuillermoCreativityApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuillermoCreativityApp
+//
+//  Created by Guillermo Ramos  on 11/3/18.
+//  Copyright © 2018 Guillermo Ramos . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
